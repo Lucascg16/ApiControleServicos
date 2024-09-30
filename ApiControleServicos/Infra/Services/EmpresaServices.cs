098467892e221ae591fae4721150ad6b25340fbe
Lucascg <lucascgiacomin16@gmail.com>
2024-09-30 20:15:48 -0300

    Adicionando biblioteca utilitaria

--- ApiControleServicos/Infra/Services/EmpresaServices.cs
+++ ApiControleServicos/Infra/Services/EmpresaServices.cs
@@ -14,15 +14,7 @@ namespace ApiControleServicos.Infra
 
 		public async Task<int> Create(CreateEmpresaModel novaEmpresa)
 		{
-			string normalizedCNPJ = "", normalizedCPF = "";
-
-			if (novaEmpresa.Cnpj is not null)
-				normalizedCNPJ = NormalizeCnpj(novaEmpresa.Cnpj);
-
-			if (novaEmpresa.Cpf is not null)
-				normalizedCPF = NormalizeCpf(novaEmpresa.Cpf);
-
-			EmpresaModel empresa = new(novaEmpresa.Nome, normalizedCNPJ, normalizedCPF);
+			EmpresaModel empresa = new(novaEmpresa.Nome, Ultilitarios.NormalizeCnpj(novaEmpresa.Cnpj), Ultilitarios.NormalizeCpf(novaEmpresa.Cpf));
 			return await _empresaRepository.Create(empresa);
 		}
 
@@ -47,21 +39,11 @@ namespace ApiControleServicos.Infra
 			if (empresa.Id == 0)
 				return;
 
-			empresa.UpdateEmpresa(empresaNova.Nome, NormalizeCnpj(empresaNova.Cnpj), NormalizeCpf(empresaNova.Cpf));
-			_empresaRepository.Update(empresa);
+			empresa.UpdateEmpresa(empresaNova.Nome, Ultilitarios.NormalizeCnpj(empresaNova.Cnpj), Ultilitarios.NormalizeCpf(empresaNova.Cpf));
+
+            _empresaRepository.Update(empresa);
 		}
 
-		public string? NormalizeCnpj(string cnpj)
-		{
-			if (!string.IsNullOrWhiteSpace(cnpj))
-				return cnpj.Replace(".", "").Replace("-", "").Replace("/", "");
-			return null;
-        }
-		public string? NormalizeCpf(string cpf)
-		{
-			if (!string.IsNullOrWhiteSpace(cpf))
-				return cpf.Replace(".", "").Replace("-", "");
-			return null;
-        }
+
 	}
 }
---
+++ ApiControleServicos/Infra/Ultilitarios.cs
@@ -0,0 +1,18 @@
+﻿namespace ApiControleServicos.Infra
+{
+    public static class Ultilitarios
+    {
+        public static string? NormalizeCnpj(string cnpj)
+        {
+            if (!string.IsNullOrWhiteSpace(cnpj))
+                return cnpj.Replace(".", "").Replace("-", "").Replace("/", "");
+            return null;
+        }
+        public static string? NormalizeCpf(string cpf)
+        {
+            if (!string.IsNullOrWhiteSpace(cpf))
+                return cpf.Replace(".", "").Replace("-", "");
+            return null;
+        }
+    }
+}
