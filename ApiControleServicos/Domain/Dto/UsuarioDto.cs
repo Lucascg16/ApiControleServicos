f998d13adf13c00b45464fac7a30da9dfd416708
Lucascg16 <lucascgiacomin16@gmail.com>
2025-03-08 14:40:55 -0300

    Corrigindo retorno da UserRoleEnum

--- ApiControleServicos/Domain/Dto/UsuarioDto.cs
+++ ApiControleServicos/Domain/Dto/UsuarioDto.cs
@@ -6,7 +6,7 @@
 		public Guid VId { get; set; }
 		public string? Nome { get; set; }
 		public string? Email { get; set; }
-		public string Role { get; set; } = "none";
+		public UserRoleEnum Role { get; set; } = UserRoleEnum.None;
 		public int EmpresaId { get; set; }
 		public bool Dono {  get; set; }
 	}
